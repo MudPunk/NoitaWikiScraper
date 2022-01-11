5d041b5638155f9dcb0237df5fb557b892a39cd6
Brian Nowak <Brian.Nowak@vermont.gov>
2022-01-11 14:58:48 -0500

    Finally figured out the whole async / await / new promise thing. No idea if this is how I should do it, but it works, so I'm callin' it done for now.

--- Scrapers/scrapeMain.js
+++ Scrapers/scrapeMain.js
@@ -1,179 +1,193 @@
 const requestPromise= require("request-promise");
 const cheerio= require("cheerio");
 const fs = require('fs');
+const { Console } = require("console");
 fs.truncate('out.csv', () => { });
 fs.appendFile('out.csv', 'Spell,Uses,Mana drain,Radius,Spread (DEG),Speed,Lifetime,Cast delay (s),Recharge time (s),Spread Modifier (DEG),Speed modifier,Lifetime Modifier,Bounces,Critical chance (%),Damage (Projectile), Damage (Explosion), Damage (Melee), Damage (Electric), Damage (Fire), Damage (Slice), Damage (Ice), Damage (Drill), Has Piercing (subject to review)'+'\r\n', 
     function (err) {
         if (err) throw err;
         else console.log('CSV cleared, Header reapplied.');
     });
-var spellData = [];
-var spellHREF = [];
-var spellInfoArr = [];
-var spellDamageLabelArr = [];
-var spellDamageValueArr = [];
+
+/*
+# References
+
+## Helpful links
+
+* https://stackoverflow.com/questions/44498314/promise-resolve-returning-2-arrays-one-undefined
+    * Return object of arrays from promise
+
+*/
 
 module.exports = {
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    scrapePrimaryUrl: async function(urlToScrape) {
 
-scrapePrimaryUrl: function(urlToScrape) { 
+        var spellData = [];
+        var spellHREF = [];
 
-    requestPromise(urlToScrape, (error, response, html) => {
-        if(!error && response.statusCode==200) {
-            const $= cheerio.load(html);
-            var http_or_https = (urlToScrape.indexOf("https") == 0 ? "https:" : "http:" );
-            const datarow= $(".cargoTable");                
+        const promise = new Promise (
+            (resolve) => {
+                requestPromise(
+                    urlToScrape, (error, response, html) => {
+                        if(!error && response.statusCode==200) {
+                            const $= cheerio.load(html);
+                            var http_or_https = (urlToScrape.indexOf("https") == 0 ? "https:" : "http:" );
+                            const datarow= $(".cargoTable");
 
-            $("tr").each((i, data) => {     
-                var csvDataRow = $(data).text().replaceAll('\n', ',');
+                            $("tr").each((i, data) => {
+                                var csvDataRow = $(data).text().replaceAll('\n', ',');
 
-                if(csvDataRow.toString().includes('Spread Modifier (DEG)')) { //just looking for anything unique to ID the header rows. Not fully sure how to just see if it's the <th tag.
-                    
-                } else {
-                    spellData.push(csvDataRow.substring(1,csvDataRow.length-1));
-                    var hrefBase = $("a").attr("href");
-                    var href = ($(data).find("td > a").attr("href") === undefined ? "" : $(data).find("td > a").attr("href"));
-                    spellHREF.push(http_or_https + hrefBase + href);
-                } 
-            }) // end $("tr").each((i, data)                       
-        
-        } // end if(!error && response.statusCode==200) 
-    }); // end requestPromise(urlToScrape, (error, response, html)    
+                                if(csvDataRow.toString().includes('Spread Modifier (DEG)')) { //just looking for anything unique to ID the header rows. Not fully sure how to just see if it's the <th tag.
 
-    return new Promise(slowYourAssDown => {
-            setTimeout(() => {slowYourAssDown(spellData);}, 300);
-        }
-    );  
-}, 
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-scrapeSecondaryUrl: function(urlToScrape) { 
-    
-    spellDamageLabelArr = [];
-    spellDamageValueArr = [];
-    spellInfoArr = [];
-
-    requestPromise(urlToScrape, (error, response, html) => {
-        if(!error && response.statusCode==200) {
-            const $= cheerio.load(html);
-
-            var http_or_https = (urlToScrape.indexOf("https") == 0 ? "https" : "http" );
-            const datarow= $("div.wds-tab__content.wds-is-current");
-           
-            var regExPattern = /^(damage).*(.*[^0-9]$)/; //find strings that end with 0-9, and ignore them. ^ asserts position at start of a line. $ asserts position at the end of a line.
-
-            var spellDivArr = [];
-            spellDivArr = $('div.pi-item.pi-data').get().map(x1 => $(x1).attr('data-source')).filter(e1 => regExPattern.test(e1));
-            $("div.pi-item.pi-data").each((i, data) => {
-                var spellDamageLabel = "";
-                var spellDamageValue = "";
-                var damageType = "";
-
-                damageType = $(data).get().map(x2 => $(x2).attr('data-source'));
-                for (var xxx = 0; xxx<spellDivArr.length; xxx++) {
-                    if (spellDivArr[xxx] == damageType) {
-                        spellDamageLabel = $(data).find(".pi-data-label").clone().children().remove().end().text().trim();
-                        spellDamageValue = $(data).find(".pi-data-value").clone().children().remove().end().text().trim();
-
-                        spellDamageLabelArr.push(spellDamageLabel);
-                        spellDamageValueArr.push(spellDamageValue);                            
-                        spellInfoArr.push(spellDamageLabel + "," + spellDamageValue);
+                                } else {
+                                    spellData.push(csvDataRow.substring(1,csvDataRow.length-1));
+                                    var hrefBase = $("a").attr("href");
+                                    var href = ($(data).find("td > a").attr("href") === undefined ? "" : $(data).find("td > a").attr("href"));
+                                    spellHREF.push(http_or_https + hrefBase + href);
+                                    
+                                }
+                            }) 
+                            resolve({spellHREF_Return: spellHREF, spellData_Return: spellData}); //returns object that contains the two arrays
+                        } //if(!error && response.statusCode==200) {
                     }
-                 }
-            });      
-        }
-    });   
+                ) //requestPromise
+            }
+        );
 
-    return new Promise(slowYourAssDown => {
-            setTimeout(() => {slowYourAssDown(spellDamageLabelArr);}, 300);
-        }
-    );  
-},
+        const spellHREFArrReturn = await promise;
+        return spellHREFArrReturn;
+    },
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    scrapeSecondaryUrl: async function(urlToScrape) {
 
-scrapePierceText: function(urlToScrape) { 
-    var csvDataRow = '';
-    var returnVal = '';
-    
-    requestPromise(urlToScrape, (error, response, html) => {
-        if(!error && response.statusCode==200) {
-            const $= cheerio.load(html);
-            var http_or_https = (urlToScrape.indexOf("https") == 0 ? "https:" : "http:" );
-            const datarow= $("div.mw-parser-output");                
+        spellDamageLabelArr = [];
+        spellDamageValueArr = [];
+        spellInfoArr = [];
 
-            $("ul").each((i, data) => {     
-                csvDataRow = $(data).find("li").clone().children().remove().end().text().trim();
-                if (csvDataRow.indexOf("piercing") > 0 || csvDataRow.indexOf("piercing") > 0) {
-                    returnVal = csvDataRow;                    
-                    console.log(csvDataRow);
-                }                
-            }) 
-        } 
-    }); 
+        const promise = new Promise (
+            (resolve) => {
+                requestPromise(
+                    urlToScrape, (error, response, html) => {
+                        if(!error && response.statusCode==200) {
+                            const $= cheerio.load(html);
 
-    return new Promise(
-        slowYourAssDown => {setTimeout(() => {slowYourAssDown(returnVal);}, 300);
+                            var http_or_https = (urlToScrape.indexOf("https") == 0 ? "https" : "http" );
+                            const datarow= $("div.wds-tab__content.wds-is-current");
 
-        }
-    );  
-},
+                            var regExPattern = /^(damage).*(.*[^0-9]$)/; //find strings that end with 0-9, and ignore them. ^ asserts position at start of a line. $ asserts position at the end of a line.
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                            var spellDivArr = [];
+                            spellDivArr = $('div.pi-item.pi-data').get().map(x1 => $(x1).attr('data-source')).filter(e1 => regExPattern.test(e1));
+                            $("div.pi-item.pi-data").each(
+                                (i, data) => {
+                                    var spellDamageLabel = "";
+                                    var spellDamageValue = "";
+                                    var damageType = "";
 
-scrapeINIT: async function() {
+                                    damageType = $(data).get().map(x2 => $(x2).attr('data-source'));
+                                    for (var xxx = 0; xxx<spellDivArr.length; xxx++) {
+                                        if (spellDivArr[xxx] == damageType) {
+                                            spellDamageLabel = $(data).find(".pi-data-label").clone().children().remove().end().text().trim();
+                                            spellDamageValue = $(data).find(".pi-data-value").clone().children().remove().end().text().trim();
 
-    var scrapePrimaryUrlArr = [];
-    
-    scrapePrimaryUrlArr = await module.exports.scrapePrimaryUrl('https://noita.fandom.com/wiki/Spell_Information_Table');
-    
-    var dmgString = 'Damage (Projectile),Damage (Explosion),Damage (Melee),Damage (Electric),Damage (Fire),Damage (Slice),Damage (Ice),Damage (Drill)';
-    
-    var scrapeSecondaryUrlArr = [];
-    
-    var scrapePierceTextText = '';
+                                            spellDamageLabelArr.push(spellDamageLabel);
+                                            spellDamageValueArr.push(spellDamageValue);
+                                            spellInfoArr.push(spellDamageLabel + "," + spellDamageValue);
+                                            //console.log(spellDamageLabel + "," + spellDamageValue);
+                                        }
+                                    }
+                                } // (i, data)
+                            );
+                            resolve({spellDamageLabelArr_Return: spellDamageLabelArr, spellDamageValueArr_Return: spellDamageValueArr});
+                        } //if(!error && response.statusCode==200)
+                    }
+                ) //requestPromise
+            }
+        );
+        const spellInfoArrReturn = await promise;
+        return spellInfoArrReturn;
+    },
 
-    for (var primaryUrlIndex = 0; primaryUrlIndex < scrapePrimaryUrlArr.length; primaryUrlIndex++) {
-
-        dmgString = 'Damage (Projectile),Damage (Explosion),Damage (Melee),Damage (Electric),Damage (Fire),Damage (Slice),Damage (Ice),Damage (Drill)';
+    scrapePierceText: async function(urlToScrape) { 
+        var csvDataRow = '';
+        var returnVal = '';
         
-        scrapeSecondaryUrlArr = await module.exports.scrapeSecondaryUrl(spellHREF[primaryUrlIndex]);
-        scrapePierceTextText = await module.exports.scrapePierceText(spellHREF[primaryUrlIndex]);
+        const promise = new Promise (
+            (resolve) => {
+            requestPromise(urlToScrape, (error, response, html) => {
+                if(!error && response.statusCode==200) {
+                    const $= cheerio.load(html);
+                    var http_or_https = (urlToScrape.indexOf("https") == 0 ? "https:" : "http:" );
+                    const datarow= $("div.mw-parser-output");                
+        
+                    $("ul").each((i, data) => {     
+                        csvDataRow = $(data).find("li").clone().children().remove().end().text().trim();
+                        if (csvDataRow.indexOf("piercing") > 0 || csvDataRow.indexOf("piercing") > 0) {
+                            returnVal = csvDataRow;                      
+                        }                
+                    }) 
+                    resolve(returnVal);
+                } // if(!error && response.statusCode==200)
+            })
+        }); 
+        const returnValReturn = await promise;
+        return returnValReturn;
+    },
 
-        for (var dmgValIndex=0; dmgValIndex<scrapeSecondaryUrlArr.length;dmgValIndex++) {
-            if(spellDamageLabelArr[dmgValIndex]=='Damage (Projectile)')
-                dmgString = dmgString.replace('Damage (Projectile)', spellDamageValueArr[dmgValIndex]);
-            if(spellDamageLabelArr[dmgValIndex]=='Damage (Explosion)')
-                dmgString = dmgString.replace('Damage (Explosion)', spellDamageValueArr[dmgValIndex]);
-            if(spellDamageLabelArr[dmgValIndex]=='Damage (Melee)')
-                dmgString = dmgString.replace('Damage (Melee)', spellDamageValueArr[dmgValIndex]);
-            if(spellDamageLabelArr[dmgValIndex]=='Damage (Electric)')
-                dmgString = dmgString.replace('Damage (Electric)', spellDamageValueArr[dmgValIndex]);
-            if(spellDamageLabelArr[dmgValIndex]=='Damage (Fire)')
-                dmgString = dmgString.replace('Damage (Fire)', spellDamageValueArr[dmgValIndex]);
-            if(spellDamageLabelArr[dmgValIndex]=='Damage (Slice)')
-                dmgString = dmgString.replace('Damage (Slice)', spellDamageValueArr[dmgValIndex]);
-            if(spellDamageLabelArr[dmgValIndex]=='Damage (Ice)')
-                dmgString = dmgString.replace('Damage (Ice)', spellDamageValueArr[dmgValIndex]);
-            if(spellDamageLabelArr[dmgValIndex]=='Damage (Drill)')
-                dmgString = dmgString.replace('Damage (Drill)', spellDamageValueArr[dmgValIndex]);
+    scrapeINIT: async function() {
+        
+        scrapePrimaryUrlArr = await module.exports.scrapePrimaryUrl('https://noita.fandom.com/wiki/Spell_Information_Table');
+        
+        var scrapePierceTextText = '';
+
+        var dmgString = 'Damage (Projectile),Damage (Explosion),Damage (Melee),Damage (Electric),Damage (Fire),Damage (Slice),Damage (Ice),Damage (Drill)';
+        
+        for(var x = 0; x < scrapePrimaryUrlArr.spellHREF_Return.length; x++) {
+
+            scrapeSecondaryUrlArr = await module.exports.scrapeSecondaryUrl(scrapePrimaryUrlArr.spellHREF_Return[x]);
+            scrapePierceTextText = await module.exports.scrapePierceText(scrapePrimaryUrlArr.spellHREF_Return[x]);
+
+            dmgString = 'Damage (Projectile),Damage (Explosion),Damage (Melee),Damage (Electric),Damage (Fire),Damage (Slice),Damage (Ice),Damage (Drill)';
+            for (var dmgValIndex=0; dmgValIndex<scrapeSecondaryUrlArr.spellDamageLabelArr_Return.length;dmgValIndex++) {
+                
+                if(scrapeSecondaryUrlArr.spellDamageLabelArr_Return[dmgValIndex]=='Damage (Projectile)')
+                    dmgString = dmgString.replace('Damage (Projectile)', scrapeSecondaryUrlArr.spellDamageValueArr_Return[dmgValIndex]);
+
+                if(scrapeSecondaryUrlArr.spellDamageLabelArr_Return[dmgValIndex]=='Damage (Explosion)')
+                    dmgString = dmgString.replace('Damage (Explosion)', scrapeSecondaryUrlArr.spellDamageValueArr_Return[dmgValIndex]);
+
+                if(scrapeSecondaryUrlArr.spellDamageLabelArr_Return[dmgValIndex]=='Damage (Melee)')
+                    dmgString = dmgString.replace('Damage (Melee)', scrapeSecondaryUrlArr.spellDamageValueArr_Return[dmgValIndex]);
+
+                if(scrapeSecondaryUrlArr.spellDamageLabelArr_Return[dmgValIndex]=='Damage (Electric)')
+                    dmgString = dmgString.replace('Damage (Electric)', scrapeSecondaryUrlArr.spellDamageValueArr_Return[dmgValIndex]);
+
+                if(scrapeSecondaryUrlArr.spellDamageLabelArr_Return[dmgValIndex]=='Damage (Fire)')
+                    dmgString = dmgString.replace('Damage (Fire)', scrapeSecondaryUrlArr.spellDamageValueArr_Return[dmgValIndex]);
+
+                if(scrapeSecondaryUrlArr.spellDamageLabelArr_Return[dmgValIndex]=='Damage (Slice)')
+                    dmgString = dmgString.replace('Damage (Slice)', scrapeSecondaryUrlArr.spellDamageValueArr_Return[dmgValIndex]);
+
+                if(scrapeSecondaryUrlArr.spellDamageLabelArr_Return[dmgValIndex]=='Damage (Ice)')
+                    dmgString = dmgString.replace('Damage (Ice)', scrapeSecondaryUrlArr.spellDamageValueArr_Return[dmgValIndex]);
+
+                if(scrapeSecondaryUrlArr.spellDamageLabelArr_Return[dmgValIndex]=='Damage (Drill)')
+                    dmgString = dmgString.replace('Damage (Drill)', scrapeSecondaryUrlArr.spellDamageValueArr_Return[dmgValIndex]);
+            
+            }
+    
+            dmgString = dmgString.replace('Damage (Projectile)', '');
+            dmgString = dmgString.replace('Damage (Explosion)', '');
+            dmgString = dmgString.replace('Damage (Melee)', '');
+            dmgString = dmgString.replace('Damage (Electric)', '');
+            dmgString = dmgString.replace('Damage (Fire)', '');
+            dmgString = dmgString.replace('Damage (Fire)', '');
+            dmgString = dmgString.replace('Damage (Slice)', '');
+            dmgString = dmgString.replace('Damage (Ice)', '');
+            dmgString = dmgString.replace('Damage (Drill)', '');
+            
+            fs.appendFile('out.csv', scrapePrimaryUrlArr.spellData_Return[x] + ',' + dmgString + ',' + (scrapePierceTextText.length > 0 ? 'True' : 'False') + '\r\n', function (err) { if (err) throw err; /*console.log('Saved!');*/ });
         }
-
-        dmgString = dmgString.replace('Damage (Projectile)', '');
-        dmgString = dmgString.replace('Damage (Explosion)', '');
-        dmgString = dmgString.replace('Damage (Melee)', '');
-        dmgString = dmgString.replace('Damage (Electric)', '');
-        dmgString = dmgString.replace('Damage (Fire)', '');
-        dmgString = dmgString.replace('Damage (Fire)', '');
-        dmgString = dmgString.replace('Damage (Slice)', '');
-        dmgString = dmgString.replace('Damage (Ice)', '');
-        dmgString = dmgString.replace('Damage (Drill)', '');
-
-        fs.appendFile('out.csv', scrapePrimaryUrlArr[primaryUrlIndex] + ',' + dmgString + ',' + (scrapePierceTextText.length > 0 ? 'True' : 'False') + '\r\n', function (err) { if (err) throw err; /*console.log('Saved!');*/ });
-    }  
-}
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////   
-
+    }
 };
